833f53c6994fb4380e794d413ed1924351965773
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-07-12 19:53:20 +0530

    [sanjana] Add :Add Data

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -9,3 +9,9 @@ Name VarChar(30),
 Salary Bigint,
 StartDate Date
 )
+--UC3
+Insert into employee_payroll(Name,Salary, StartDate) values('Sanjana','10000','2021-07-15');
+Insert into employee_payroll(Name,Salary, StartDate) values('Radha','20000','2021-07-16');
+Insert into employee_payroll(Name,Salary, StartDate) values('Rutuja','30000','2021-07-17');
+Insert into employee_payroll(Name,Salary, StartDate) values('Smita','40000','2021-07-18');
+Insert into employee_payroll(Name,Salary, StartDate) values('Pranali','50000','2021-07-19');
