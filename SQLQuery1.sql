446bd7bf5cf11104dac136285e404716c6bb957d
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-07-12 19:51:23 +0530

    [sanjana] Add :Create table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,11 @@
 --UC1
 Create Database payroll_service;
 Use payroll_service;
+--UC2
+Create table employee_payroll
+(
+ID int Primary Key identity(1,1),
+Name VarChar(30),
+Salary Bigint,
+StartDate Date
+)
