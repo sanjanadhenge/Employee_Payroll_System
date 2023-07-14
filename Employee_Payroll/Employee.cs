6708cf6de23af9a91904b42b807a8c693e3a9056
sanjanadhenge <dhengesanjana2001@gmail.com>
2023-07-14 16:03:49 +0530

    [sanjana] Add :Create Database

---
+++ Employee_Payroll/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee_Payroll
+{
+    internal class Employee
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public long Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public long BasicPay { get; set; }
+        public long Deductions { get; set; }
+        public long TaxablePay { get; set; }
+        public long Incometax { get; set; }
+        public long NetPay { get; set; }
+    }
+}
